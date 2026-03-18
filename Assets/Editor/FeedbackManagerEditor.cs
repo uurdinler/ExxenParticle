5470f4858e9e57f7d45232cbbe35c4b4a1e566a1
Uğur Dinler <ugurdinler@gmail.com>
2026-03-18 18:16:12 +0300

    initial

---
+++ Assets/Editor/FeedbackManagerEditor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(FeedbackManager))]
+public class FeedbackManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        // Standart Inspector alanlarını çiz (Listeleri görmeye devam etmen için)
+        DrawDefaultInspector();
+
+        FeedbackManager manager = (FeedbackManager)target;
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Test Kontrolleri", EditorStyles.boldLabel);
+
+        // Yanlış Butonu (Biraz ayırmak için rengini değiştirebilirsin)
+        GUI.backgroundColor = Color.red;
+        if (GUILayout.Button("Play Yanlış"))
+        {
+            manager.PlayYanlis();
+        }
+
+        // Doğru Butonları
+        GUI.backgroundColor = Color.green;
+        if (GUILayout.Button("Play Doğru V1")) manager.PlayDogruV1();
+        if (GUILayout.Button("Play Doğru V2")) manager.PlayDogruV2();
+        if (GUILayout.Button("Play Doğru V3")) manager.PlayDogruV3();
+        if (GUILayout.Button("Play Doğru V4")) manager.PlayDogruV4();
+
+        GUI.backgroundColor = Color.white; // Rengi sıfırla
+        if (GUILayout.Button("Hepsini Kapat"))
+        {
+            // StopAllFeedbacks private ise bunu public yapman gerekebilir
+            // Veya yeni bir public metod ekleyebilirsin
+            manager.SendMessage("StopAllFeedbacks");
+        }
+    }
+}
---
+++ Assets/FeedbackSystem/FeedbackManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedbackManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> yanlis;
+    [SerializeField] private List<GameObject> dogruV1;
+    [SerializeField] private List<GameObject> dogruV2;
+    [SerializeField] private List<GameObject> dogruV3;
+    [SerializeField] private List<GameObject> dogruV4;
+
+
+    private void PlayFeedback(List<GameObject> feedbackList)
+    {
+        StopAllFeedbacks();
+        feedbackList.ForEach(go => { if (go != null) go.SetActive(true); });
+    }
+
+    private void StopAllFeedbacks()
+    {
+        // Alt alta hızlı çözüm
+        yanlis.ForEach(go => { if (go != null) go.SetActive(false); });
+        dogruV1.ForEach(go => { if (go != null) go.SetActive(false); });
+        dogruV2.ForEach(go => { if (go != null) go.SetActive(false); });
+        dogruV3.ForEach(go => { if (go != null) go.SetActive(false); });
+        dogruV4.ForEach(go => { if (go != null) go.SetActive(false); });
+    }
+
+    // --- Play Metodları ---
+
+    public void PlayYanlis() => PlayFeedback(yanlis);
+    public void PlayDogruV1() => PlayFeedback(dogruV1);
+    public void PlayDogruV2() => PlayFeedback(dogruV2);
+    public void PlayDogruV3() => PlayFeedback(dogruV3);
+    public void PlayDogruV4() => PlayFeedback(dogruV4);
+}
